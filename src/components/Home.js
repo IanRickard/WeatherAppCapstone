18242da191614153ca871690ae435fffdf48dd97
Xiaoying-Huang <hxybecomexs@gmail.com>
2022-05-27 13:14:06 +0100

    change text for weather planner

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ export default function Home(){
   return (
     <div>
       <h2>Location: Check out the weather on popular tourist destionations in the world!</h2>
-      <h2>Planner: Choose the city you like and get 3-hourly weather forecast for the next day!</h2>
+      <h2>Planner: Choose the city you like and get 3-hourly weather forecast for the next 24 hours!</h2>
       <h2>Happy Exploring!</h2>
     </div>
   )
