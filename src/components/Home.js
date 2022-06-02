9a27d8a8153bb57820eb4bd766722cc5f02eb898
IanRickard <36732676+IanRickard@users.noreply.github.com>
2022-06-02 13:09:07 +0100

    CSS and HTML Touches

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Home(){
   return (
     <div>
-      <h2>Location: Check out the weather on popular tourist destionations in the world!</h2>
-      <h2>Planner: Choose the city you like and get 3-hourly weather forecast for the next 24 hours!</h2>
-      <h2>Happy Exploring!</h2>
+      <p><h2>Location: Check out the weather on popular tourist destionations in the world!</h2></p>
+      <p><h2>Planner: Choose the city you like and get 3-hourly weather forecast for the next 24 hours!</h2></p>
+      <p><h2>Happy Exploring!</h2></p>
     </div>
   )
 }
